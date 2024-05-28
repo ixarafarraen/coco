57ce76cdbbd711ea956efe0a295da86d66805b76
ixarafarraen <ixarafarraen@gmail.com>
2024-05-28 12:21:30 +0100

    update

---
+++ home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+a = st.button('test')
+b = st.button('caty')
+
+if a:
+    st.write('cinnadmokrolpl')
+
+
+if b:
+    st.write('cocox')
+
+
+
+
