ab0e48e14dc7fc3fbdb5808625cb095170c2916a
ixarafarraen <ixarafarraen@gmail.com>
2024-05-28 13:16:04 +0100

    update

--- home.py
+++ home.py
@@ -4,17 +4,21 @@ import streamlit as st
 st.header('fredom fighters')
 name = st.radio(
     "what was your name last time we met:",
-    ["athychew","mager cat","sofia","amilya","nicol","none of the above"])
+    ["athychew","mager cat","sofia","amilya","nicol","none of the above"],
+    index=5)
 
 if name == "none of the above":
     st.write("get lost ")
     st.image("getlost.jpeg")
 else:
     st.write("welcome "+name)
+    st.image("member.webp")
 
 
-
-
+    topic = st.radio(
+        "pike a topic :",
+        ["fighting ","masc","wepon"],
+        index=0)
 
 
 A, col2, col3 = st.columns(3)
